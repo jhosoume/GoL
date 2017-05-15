09bef4baeb51858c29ba327f859e780cf8b3edb5
Juliana Hosoume <ju.hosoume@gmail.com>
2017-05-14 22:16:24 -0300

    Minor fixes to prepare to add new features

---
+++ GoL/src/GoL/DerivationStrategy.scala
@@ -0,0 +1,12 @@
+package GoL
+
+/**
+  */
+trait DerivationStrategy {
+  /* verifica se uma celula deve ser mantida viva */
+  def shouldKeepAlive(line: Int, column: Int): Boolean
+
+  /* verifica se uma celula deve (re)nascer */
+  def shouldRevive(line: Int, column: Int): Boolean
+
+}
---
+++ GoL/src/GoL/GameController.scala
@@ -0,0 +1,45 @@
+package GoL
+
+/**
+ * Relaciona o componente View com o componente Model. 
+ * 
+ * @author Breno Xavier (baseado na implementacao Java de rbonifacio@unb.br
+ */
+object GameController {
+  
+  def start {
+    GameView.update
+  }
+  
+  def halt() {
+    //oops, nao muito legal fazer sysout na classe Controller
+    println("\n \n")
+    Statistics.display
+    System.exit(0)
+  }
+
+  def makeCellAlive(line: Int, column: Int) {
+    try {
+			GameEngine.makeCellAlive(line, column)
+			GameView.update
+		}
+		catch {
+		  case ex: IllegalArgumentException => {
+		    println(ex.getMessage)
+		  }
+		}
+  }
+  
+  def nextGeneration {
+    GameEngine.nextGeneration
+    GameView.update
+  }
+
+  def gensUntilTotalDeath {
+    while (GameEngine.numberOfAliveCells > 0) {
+      GameEngine.nextGeneration
+      GameView.update
+    }
+  }
+  
+}
